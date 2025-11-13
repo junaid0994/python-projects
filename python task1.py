1d62e6d8d76e98e636fdb2e29ed8c7e1cf862aa6
junaid0994 <junaidashraf0994@gmail.com>
2025-11-13 20:53:05 +0530

    Add files via upload

---
+++ python task1.py
@@ -0,0 +1,26 @@
+# Command Line Calculator
+
+print("Simple Calculator")
+print("Operations: +  -  *  /")
+
+# Take user input
+num1 = float(input("Enter first number: "))
+op = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+# Perform calculation
+if op == '+':
+    result = num1 + num2
+elif op == '-':
+    result = num1 - num2
+elif op == '*':
+    result = num1 * num2
+elif op == '/':
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Error! Division by zero."
+else:
+    result = "Invalid operator!"
+
+print("Result:", result)
